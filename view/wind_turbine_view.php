0e62743a6511dd87aeed3beade67cadb87e7432d
Rachel Westwater <rachel-westwater@hotmail.com>
2020-12-01 16:27:26 +0000

    adding skeleton head and body to view

---
+++ view/wind_turbine_view.php
@@ -0,0 +1,10 @@
+<DOCTYPE html>
+    <html lang='en'>
+    <head>
+        <meta charset='UFT-8'>
+        <title> Cyberhawk Turbine Challenge </title>
+    </head>
+
+    <body>
+        <?php echo '<h1> Hello World! </h1>' ?>    
+    </body>
