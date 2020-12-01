9678c2b0a3140355b7cfb0c655daf8f6b7a2906f
Rachel Westwater <rachel-westwater@hotmail.com>
2020-12-01 16:04:33 +0000

    adding basic functionality to wind_turbine

--- model/wind_turbine.php
+++ model/wind_turbine.php
@@ -1,6 +1,29 @@
 <?php 
 
 function windTurbine() {
-    // start here
+    // loop through items 1 to 100.
+    foreach (range(1, 100) as $item) {
+
+        // if item can be divided by both 3 and 5, coating damage and lightning strike.
+        if ($item % 3 == 0 && $item % 5 == 0) {
+            echo ("Oh no! We have coating damage and a lightning strike! \n");
+        } 
+
+        // if item can be divided by 3, coating damage.
+        elseif ($item % 3 == 0) {
+            echo ("Coating damage! \n");
+        }
+
+        // if item can be divided by 5, lightning stike.
+        elseif ($item % 5 == 0) {
+            echo ("Lightning strike! \n");
+        }
+        
+        // else just print the item.
+        else {
+            echo ($item . "\n");
+        }
+    }
 }
-?> 
+
+windTurbine();
