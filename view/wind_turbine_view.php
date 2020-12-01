ed4a559a4f925cc731855bf51c598dc31ce85714
Rachel Westwater <rachel-westwater@hotmail.com>
2020-12-01 19:53:49 +0000

    adding ajax call to render results on same page

--- view/wind_turbine_view.php
+++ view/wind_turbine_view.php
@@ -1,17 +1,18 @@
 <!--
 Author: Rachel Westwater
-Embedding PHP within a HTML 
-file to allow the PHP to be 
+Using HTML with inline CSS and ajax
+to allow the PHP to be 
 rendered in a browser 
 -->
 
 <DOCTYPE html>
-    <html lang='en'>
+    <html lang = 'en'>
     <head>
         
-        <meta charset='UTF-8'>
+        <meta charset = 'UTF-8'>
         <title> Cyberhawk Turbine Challenge </title>
-        <link rel='shortcut icon' type='image/x-icon' href='/assets/turbine_favicon.png' />
+
+        <link rel = 'shortcut icon' type = 'image/x-icon' href = 'favicon.ico' />
         
         <style>
             body {
@@ -27,9 +28,29 @@ rendered in a browser
     </head>
 
     <body>
-        <h1 style="text-align: center;" > Cyberhawk Turbine Challenge </h1>
-        <p style="text-align: center;">Spin the wind turbine to inspect the items!</p>
-        <form action="/model/wind_turbine.php">
-            <input type="submit" value="Spin Wind Turbine" style="display: block; margin: 0 auto;"/> <!--centers submit button-->
-        </form>  
+        <h1 style = "text-align: center;" style = "backdrop-filter: blur(100px);" > Cyberhawk Turbine Challenge </h1>
+        <p style = "text-align: center; margin: 10px;">Spin the wind turbine to inspect the items!</p>
+        
+        <!-- click button options -->
+        <form>
+            <input type = "button" value = "Spin Wind Turbine" id = "output" style = "display: block; margin: 0 auto;" onclick = "loadWindTurbine()" /> <!--centers submit button-->
+        </form>
+
+
+        <!-- using ajax to call windTurbine function from model -->
+        <script>
+            function loadWindTurbine() {
+            var xhttp = new XMLHttpRequest();
+            xhttp.onreadystatechange = function() {
+                if (this.readyState == 4 && this.status == 200) {
+                document.getElementById("display_message").innerHTML = this.responseText;
+                }
+            };
+            xhttp.open("GET", "/model/wind_turbine.php", true);
+            xhttp.send();
+            }
+        </script>
+        
+        <div id = "display_message" style = "text-align: center;" ></div>
+
     </body>
