774cd9cb3401019fa3405da543797c6dafbde04d
Rachel Westwater <rachel-westwater@hotmail.com>
2020-12-01 19:52:48 +0000

    adding line breaks in model and removing old favi location

--- model/wind_turbine.php
+++ model/wind_turbine.php
@@ -6,22 +6,22 @@ function windTurbine() {
 
         // if item can be divided by both 3 and 5, coating damage and lightning strike.
         if ($item % 3 == 0 && $item % 5 == 0) {
-            echo ("Oh no! We have coating damage and a lightning strike! \n");
+            echo nl2br("Oh no! We have coating damage and a lightning strike! \n");
         } 
 
         // if item can be divided by 3, coating damage.
         elseif ($item % 3 == 0) {
-            echo ("Coating damage! \n");
+            echo nl2br("Coating damage! \n");
         }
 
         // if item can be divided by 5, lightning stike.
         elseif ($item % 5 == 0) {
-            echo ("Lightning strike! \n");
+            echo nl2br("Lightning strike! \n");
         }
-        
+
         // else just print the item.
         else {
-            echo ($item . "\n");
+            echo nl2br($item . "\n");
         }
     }
 }
