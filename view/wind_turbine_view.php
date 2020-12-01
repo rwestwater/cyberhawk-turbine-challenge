f275a14183cbe198ef2c00104a57864faad1f2b0
Rachel Westwater <rachel-westwater@hotmail.com>
2020-12-01 18:33:50 +0000

    setting styling for background image and button

--- view/wind_turbine_view.php
+++ view/wind_turbine_view.php
@@ -1,17 +1,35 @@
-/**
+<!--
 Author: Rachel Westwater
 Embedding PHP within a HTML 
 file to allow the PHP to be 
 rendered in a browser 
-*/
+-->
 
 <DOCTYPE html>
     <html lang='en'>
     <head>
-        <meta charset='UFT-8'>
+        
+        <meta charset='UTF-8'>
         <title> Cyberhawk Turbine Challenge </title>
+        <link rel='shortcut icon' type='image/x-icon' href='/assets/turbine_favicon.png' />
+        
+        <style>
+            body {
+                background-image: url('/assets/cloud_background.png');
+                background-repeat: no-repeat;
+                background-attachment: fixed;
+                background-size: cover;
+                color: white;
+                font-family: verdana;
+            }
+        
+        </style>
     </head>
 
     <body>
-        <?php echo '<h1> Hello World! </h1>' ?>    
+        <h1 style="text-align: center;" > Cyberhawk Turbine Challenge </h1>
+        <p style="text-align: center;">Spin the wind turbine to inspect the items!</p>
+        <form action="/model/wind_turbine.php">
+            <input type="submit" value="Spin Wind Turbine" style="display: block; margin: 0 auto;"/> <!--centers submit button-->
+        </form>  
     </body>
